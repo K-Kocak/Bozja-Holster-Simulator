ca72bc500a08dc189a9a7a7d1d42745cb7f8ffa9
K-Kocak <kakocak@outlook.com>
2024-06-18 22:48:20 +0100

    added lost finds cache pic to project

--- src/backend/counter.tsx
+++ src/backend/counter.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
 import { useAppSelector, useAppDispatch } from './hooks';
-
-import { useSelector, useDispatch } from 'react-redux';
-
 import { decrement, increment } from './counterSlice';
 
 
