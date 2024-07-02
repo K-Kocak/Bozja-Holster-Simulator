716a5da8ca7ba3b59667e64fb3e6be4d5d49982a
K-Kocak <kakocak@outlook.com>
2024-07-02 23:39:50 +0100

    Lost Actions Div Gen file created

---
+++ src/ui/components/LostActionsDivGen.tsx
@@ -0,0 +1,4 @@
+// Divs that display when an action is hovered on is created here
+
+import LostActions from '@backend/lostactions/ActionData';
+
--- src/ui/components/LostFindsCache.tsx
+++ src/ui/components/LostFindsCache.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import '../../scss/ui/components/LostFindsCache.scss';
 
+import LostActions from '@backend/lostactions/ActionData';
+
 class LostFindsCache extends React.Component<any, never> {
     render() {
         return (
