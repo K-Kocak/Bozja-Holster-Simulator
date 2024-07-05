ae5f2deb2ca629e19a0dfc0f8fa07dd928a68ecd
K-Kocak <kakocak@outlook.com>
2024-07-05 05:45:20 +0100

    Hover div testing

--- src/ui/components/LostActionsDivGen.tsx
+++ src/ui/components/LostActionsDivGen.tsx
@@ -4,4 +4,18 @@ import React from 'react'
 
 import LostActions from '@backend/lostactions/ActionData';
 
+import LostActionsNoBorder from '../pictures/LostActions/LostActionsImgInitialise';
 
+import '@css/ui/components/LostActionsDivGen.scss';
+
+const LostActionsAsArray = [LostActions.Offensive.LostFocus, LostActions.Offensive.LostFontofMagic];
+
+const LostFocusLink = LostActionsAsArray[0].img;
+
+const LostActionInformation: React.JSX.Element[] = [<div className="Testing" style={{'background': 'no-repeat url(' + LostFocusLink + ')', 'backgroundSize': 'cover',
+    'position': 'absolute',
+    'left': '57px',
+    'top': '55px'
+}}></div>];
+console.log(LostActionsAsArray[0].img)
+export default LostActionInformation;
--- src/ui/components/LostFindsCache.tsx
+++ src/ui/components/LostFindsCache.tsx
@@ -4,11 +4,13 @@ import '../../scss/ui/components/LostFindsCache.scss';
 
 import LostActions from '@backend/lostactions/ActionData';
 
+import LostActionInformation from './LostActionsDivGen';
+
 class LostFindsCache extends React.Component<any, never> {
     render() {
         return (
             <div id="LostFindsCacheContainer">
-
+                {LostActionInformation[0]}
             </div>
         )
     }
