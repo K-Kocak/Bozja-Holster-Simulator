a695973320fe11afeb2edcae3e773b35bda3fc82
K-Kocak <kakocak@outlook.com>
2024-07-08 23:51:22 +0100

    Div Testing

--- src/ui/components/LostActionsDivGen.tsx
+++ src/ui/components/LostActionsDivGen.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 import LostActions from '@backend/lostactions/ActionData';
 
-import LostActionsNoBorder from '../pictures/LostActions/LostActionsImgInitialise';
+//import LostActionsNoBorder from '../pictures/LostActions/LostActionsImgInitialise';
 
 import '@css/ui/components/LostActionsDivGen.scss';
 
 const LostActionsAsArray = [LostActions.Offensive.LostFocus, LostActions.Offensive.LostFontofMagic];
 
-const LostFocusLink = LostActionsAsArray[0].img;
+//const LostFocusLink = LostActionsAsArray[0].img;
 
 /*<div className="Testing" style={{'background': 'no-repeat url(' + LostFocusLink + ')', 'backgroundSize': 'cover',
     'position': 'absolute',
@@ -21,21 +21,32 @@ const LostFocusLink = LostActionsAsArray[0].img;
 const LostActionInformation: React.JSX.Element[] = [<div className="Testing">
     <div>
         <div>
-            <div></div>
+            <div>Image Here</div>
             <div>
-                <span></span>
-                <span></span>
+                <span>Action Name</span>
+                <span>Action Type</span>
             </div>
         </div>
         <div>
-            <div></div>
-            <div></div>
+            <div>Range</div>
+            <div>Radius</div>
         </div>
     </div>
 
-    <div></div>
-    <div></div>
-    <div></div>
+    <div>
+        <div>Cast</div>
+        <div>Recast</div>
+        <div>Available</div>
+    </div>
+
+    <div>
+        Action Description
+    </div>
+
+    <div>
+        <div>Affinity</div>
+        <div>Jobs that can use it.</div>
+    </div>
 </div>];
 console.log(LostActionsAsArray[0].img)
 export default LostActionInformation;
