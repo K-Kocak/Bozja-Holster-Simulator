5a199aa88b98e02594622599e2a62ae17c6eebf5
K-Kocak <kakocak@outlook.com>
2024-07-10 23:21:49 +0100

    Div Work

--- src/ui/components/LostActionsDivGen.tsx
+++ src/ui/components/LostActionsDivGen.tsx
@@ -27,23 +27,23 @@ const LostActionInformation: React.JSX.Element[] = [<div className="Testing">
                 <p>{LostActionsAsArray[0].category.EN}</p>
             </div>
         </div>
-        <div>
-            <div>Range: {LostActionsAsArray[0].range}</div>
-            <div>Radius: {LostActionsAsArray[0].radius}</div>
+        <div className="Testing1Inner2">
+            <div className="Testing1Inner2Inner1">Range: {LostActionsAsArray[0].range}</div>
+            <div className="Testing1Inner2Inner2">Radius: {LostActionsAsArray[0].radius}</div>
         </div>
     </div>
 
-    <div>
-        <div>Cast: {LostActionsAsArray[0].cast}</div>
-        <div>Recast: {LostActionsAsArray[0].recast}</div>
-        <div>Available: {LostActionsAsArray[0].available}</div>
+    <div className="Testing2">
+        <div className="Testing2Inner1">Cast: {LostActionsAsArray[0].cast}</div>
+        <div className="Testing2Inner2">Recast: {LostActionsAsArray[0].recast}</div>
+        <div className="Testing2Inner3">Available: {LostActionsAsArray[0].available}</div>
     </div>
 
-    <div>
+    <div className="Testing3">
         {LostActionsAsArray[0].description.EN}
     </div>
 
-    <div>
+    <div className="Testing4">
         <div>Affinity</div>
         <div>{LostActionsAsArray[0].affinity.EN}</div>
     </div>
