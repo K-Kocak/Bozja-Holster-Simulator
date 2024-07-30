9a7421ec474c820ce026cfc6061a74e12d1f3ad7
K-Kocak <kakocak@outlook.com>
2024-07-30 23:20:38 +0100

    margin adjustments

--- src/ui/components/LostActionsDivGen.tsx
+++ src/ui/components/LostActionsDivGen.tsx
@@ -20,45 +20,47 @@ const LostActionsAsArray = [LostActions.Offensive.LostFocus, LostActions.Offensi
 
 const LostActionInformation: React.JSX.Element[] = [<div className="Testing">
     <div className="TestingGradient"></div>
-    <div className="Testing1">   
-        <div className="Testing1Inner1">
-            <div className="TestingImage"><img src={LostActionsAsArray[0].img}></img></div>
-            <div className="Testing1Inner1Inner">
-                <p className="TestingActionName">{LostActionsAsArray[0].name.EN}</p>
-                <p className="TestingActionCat">{LostActionsAsArray[0].skillType.EN}</p>
+    <div className="TestingActionEncompass">
+        <div className="Testing1">   
+            <div className="Testing1Inner1">
+                <div className="TestingImage"><img src={LostActionsAsArray[0].img}></img></div>
+                <div className="Testing1Inner1Inner">
+                    <p className="TestingActionName">{LostActionsAsArray[0].name.EN}</p>
+                    <p className="TestingActionCat">{LostActionsAsArray[0].skillType.EN}</p>
+                </div>
+            </div>
+            <div className="Testing1Inner2">
+                <div className="Testing1Inner2Inner1">Range: {LostActionsAsArray[0].range}</div>
+                <div className="Testing1Inner2Inner2">Radius: {LostActionsAsArray[0].radius}</div>
             </div>
         </div>
-        <div className="Testing1Inner2">
-            <div className="Testing1Inner2Inner1">Range: {LostActionsAsArray[0].range}</div>
-            <div className="Testing1Inner2Inner2">Radius: {LostActionsAsArray[0].radius}</div>
-        </div>
-    </div>
 
-    <div className="Testing2">
-        <div className="Testing2Inner1">
-            <p>Cast</p>
-            <p>{LostActionsAsArray[0].cast}</p>
+        <div className="Testing2">
+            <div className="Testing2Inner1">
+                <p>Cast</p>
+                <p>{LostActionsAsArray[0].cast}</p>
+            </div>
+            <div className="Testing2Inner2">
+                <p>Recast</p>
+                <p>{LostActionsAsArray[0].recast}</p>
+            </div>
+            <div className="Testing2Inner3">
+                <p>Available</p>
+                <p>{LostActionsAsArray[0].available}</p>
+            </div>
         </div>
-        <div className="Testing2Inner2">
-            <p>Recast</p>
-            <p>{LostActionsAsArray[0].recast}</p>
+        <div className="SeparatorDiv">
+            <div className="TestingUpperSeparator"></div>
+            <div className="TestingSeparator"></div>   
         </div>
-        <div className="Testing2Inner3">
-            <p>Available</p>
-            <p>{LostActionsAsArray[0].available}</p>
+        <div className="Testing3">
+            {LostActionsAsArray[1].description.EN}
         </div>
-    </div>
-    <div className="SeparatorDiv">
-        <div className="TestingUpperSeparator"></div>
-        <div className="TestingSeparator"></div>   
-    </div>
-    <div className="Testing3">
-        {LostActionsAsArray[1].description.EN}
-    </div>
 
-    <div className="Testing4">
-        <div className="Testing4Inner1">Affinity</div>
-        <div className="Testing4Inner2">{LostActionsAsArray[0].affinity.EN}</div>
+        <div className="Testing4">
+            <div className="Testing4Inner1">Affinity</div>
+            <div className="Testing4Inner2">{LostActionsAsArray[0].affinity.EN}</div>
+        </div>
     </div>
 </div>];
 console.log(LostActionsAsArray[0].img)
