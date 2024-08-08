775ddc8a4029a3927798a86f3b8dbb0cf634a3e7
K-Kocak <kakocak@outlook.com>
2024-08-08 23:54:29 +0100

    separator colour and spacing fixing to resemble ingame, pre-testing1 rearrangement (action picture, name, type etc)

--- src/ui/components/LostActionsDivGen.tsx
+++ src/ui/components/LostActionsDivGen.tsx
@@ -24,43 +24,44 @@ const LostActionInformation: React.JSX.Element[] = [<div className="Testing">
     <div className="TestingActionEncompass">
         <div className="Testing1">   
             <div className="Testing1Inner1">
-                <div className="TestingImage"><img src={LostActionsAsArray[0].img}></img></div>
+                <div className="TestingImage"><img src={LostActionsAsArray[1].img}></img></div>
                 <div className="Testing1Inner1Inner">
-                    <p className="TestingActionName">{LostActionsAsArray[0].name.EN}</p>
-                    <p className="TestingActionCat">{LostActionsAsArray[0].skillType.EN}</p>
+                    <p className="TestingActionName">{LostActionsAsArray[1].name.EN}</p>
+                    <p className="TestingActionCat">{LostActionsAsArray[1].skillType.EN}</p>
                 </div>
             </div>
             <div className="Testing1Inner2">
-                <div className="Testing1Inner2Inner1"><span>Range</span> {LostActionsAsArray[0].range}</div>
-                <div className="Testing1Inner2Inner2"><span>Radius</span> {LostActionsAsArray[0].radius}</div>
+                <div className="Testing1Inner2Inner1"><span>Range</span> {LostActionsAsArray[1].range}</div>
+                <div className="Testing1Inner2Inner2"><span>Radius</span> {LostActionsAsArray[1].radius}</div>
             </div>
         </div>
 
         <div className="Testing2">
             <div className="Testing2Inner1">
                 <p className="TestingText">Cast</p>
-                <p className="TestingText2">{LostActionsAsArray[0].cast}</p>
+                <p className="TestingText2">{LostActionsAsArray[1].cast}</p>
             </div>
             <div className="Testing2Inner2">
                 <p className="TestingText">Recast</p>
-                <p className="TestingText2">{LostActionsAsArray[0].recast}</p>
+                <p className="TestingText2">{LostActionsAsArray[1].recast}</p>
             </div>
             <div className="Testing2Inner3">
                 <p className="TestingText">Available</p>
-                <p className="TestingText2">{LostActionsAsArray[0].available}</p>
+                <p className="TestingText2">{LostActionsAsArray[1].available}</p>
             </div>
         </div>
         <div className="SeparatorDiv">
             <div className="TestingUpperSeparator"></div>
-            <div className="TestingSeparator"></div>   
+            <div className="TestingSeparator"></div>
+            <div className="TestingLowerSeparator"></div> 
         </div>
         <div className="Testing3">
-            {LostActionsAsArray[0].description.EN}
+            {LostActionsAsArray[1].description.EN}
         </div>
 
         <div className="Testing4">
             <div className="Testing4Inner1">Affinity</div>
-            <div className="Testing4Inner2">{LostActionsAsArray[0].affinity.EN}</div>
+            <div className="Testing4Inner2">{LostActionsAsArray[1].affinity.EN}</div>
         </div>
     </div>
 </div>];
