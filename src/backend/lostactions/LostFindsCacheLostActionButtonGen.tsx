2f01a678fd08ea25d17b123f7c4f4726557b80d4
K-Kocak <kakocak@outlook.com>
2024-08-23 22:26:05 +0100

    attempting to line up several actions in the lost finds cache

--- src/backend/lostactions/LostFindsCacheLostActionButtonGen.tsx
+++ src/backend/lostactions/LostFindsCacheLostActionButtonGen.tsx
@@ -5,11 +5,17 @@ import LostActionsAsObjectArray from '@backend/lostactions/ActionDataToObjectArr
 
 import CreateLostActionInformationBoxes from '@backend/lostactions/LostActionsDivGen';
 
+import '@css/ui/components/LostFindsCacheLostActionButtonGen.scss';
+
 const LostActionInformationBoxes : React.JSX.Element[] = CreateLostActionInformationBoxes(LostActionsAsObjectArray);
 
-export const TestButtonCode : React.JSX.Element = 
-<div>
-    <img src={LostActions.Offensive.LostFocus.imgBorder}></img>
-</div>
-
+export const TestButtonCode : React.JSX.Element =
+<div className="LostActionOffensive">
+    <div className="LostCacheLostActionButton">
+        <img src={LostActions.Offensive.LostFocus.imgBorder}></img>
+    </div>
+    <div className="LostCacheLostActionButton">
+        <img src={LostActions.Offensive.LostFontofMagic.imgBorder}></img>
+    </div>
+</div> 
 export default TestButtonCode;
