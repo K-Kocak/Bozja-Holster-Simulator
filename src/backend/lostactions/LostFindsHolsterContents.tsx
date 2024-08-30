52040ff24ee2288d125a0fc200ac579dc471918e
K-Kocak <kakocak@outlook.com>
2024-08-30 23:18:09 +0100

    Template creation progression, more css focus

--- src/backend/lostactions/LostFindsHolsterContents.tsx
+++ src/backend/lostactions/LostFindsHolsterContents.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import '@css/ui/components/LostFindsHolsterContents.scss';
 //import '@css/ui/components/LostActionsDivGen.scss';
 
+import LostActions from './ActionData';
+
 import FancyGraphicSymbol from '@ui/pictures/BozjaLostFindsHolsterFancyGraphicForCategory.png';
 
 import { AutomateSeparator } from '@backend/lostactions/LostActionsDivGen';
@@ -18,6 +20,22 @@ const Testing : React.JSX.Element =
             <div className="LostFindsHolsterActionCategoryFancyGraphic">
                 <img className="LostFindsHolsterActionCategoryFancyGraphicPicture" src={FancyGraphicSymbol}></img>
                 <span className="LostFindsHolsterActionCategoryName">Offensive</span>
+            </div>
+            <div className="LostFindsHolsterActionBox">
+                <div className="LostFindsHolsterActionBoxImage">
+                    <img src={LostActions.Offensive.LostFocus.imgBorder}></img>
+                </div>
+                <div className="LostFindsHolsterActionBoxName">
+                    <span>{LostActions.Offensive.LostFocus.name.EN}</span>
+                </div>
+            </div>
+            <div className="LostFindsHolsterActionBox">
+                <div className="LostFindsHolsterActionBoxImage">
+                    <img src={LostActions.Offensive.LostFontofPower.imgBorder}></img>
+                </div>
+                <div className="LostFindsHolsterActionBoxName">
+                    <span>{LostActions.Offensive.LostFontofPower.name.EN}</span>
+                </div>
             </div>
         </div>
 
