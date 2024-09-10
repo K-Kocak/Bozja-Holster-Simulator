c3c1e20b1468161788a7ccc4fe87391b3eb7ffd9
K-Kocak <kakocak@outlook.com>
2024-09-10 22:20:08 +0100

    created basic template for a set, added parameters to actionset

--- src/backend/interfaces/ILostActionSet.ts
+++ src/backend/interfaces/ILostActionSet.ts
@@ -1,7 +1,9 @@
 import IAction from "@backend/interfaces/IAction";
 
 export interface ILostActionSet {
+    id: number,
     name: string,
+    holsterType: string,
+    totalWeight: number,
     holsterContents: IAction[],
-    
 }
--- src/backend/lostactions/holstersets/SavedHolstersContent.tsx
+++ src/backend/lostactions/holstersets/SavedHolstersContent.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { BaseSyntheticEvent } from 'react';
+
+import LoadSetImage from '@ui/pictures/BozjaLoadSetImage81x81.png';
 
 import '@css/ui/components/SavedHolstersContent.scss';
 
 const Test = () => {
+
+    function HandleLoadSetToHolsterClick(event : BaseSyntheticEvent) {
+        console.log(event);
+    }
+
+    function HandleDeleteSetClick(event : BaseSyntheticEvent) {
+        console.log(event);
+    }
+
     return (
     <div className="SavedHolstersContainer">
         <div className="SavedHolstersTitleText">
@@ -10,6 +21,49 @@ const Test = () => {
         </div>
         <div style={{color: "white"}} className="SavedHolstersUserButtons">
             <span>PlaceHolder For Buttons</span>
+        </div>
+        <div className="TemplateSavedSetsAreaContainer">
+
+            <div className="ExampleSavedSet">
+
+                <div className="SavedHolstersLoadAndDeleteHolster">
+                    <div className="SavedHolstersLoadHolster">
+                        <img onClick={HandleLoadSetToHolsterClick} src={LoadSetImage}></img>
+                    </div>
+                    <div className="SavedHolstersDeleteHolster">
+                        <img onClick={HandleDeleteSetClick}src={LoadSetImage}></img>
+                    </div>
+                </div>
+
+                <div className="SavedHolstersTitleAndActions">
+                    <div className="SavedHolstersTitle">
+                        Title of the holster
+                    </div>
+                    <div className="SavedHolstersListActionsInSet">
+                        <div className="SavedHolstersActionInSet">
+                            An Action
+                        </div>
+                        <div className="SavedHolstersActionInSet">
+                            An Action 2
+                        </div>
+                        <div className="SavedHolstersActionInSet">
+                            An Action 3
+                        </div>
+                        <div className="SavedHolstersActionInSet">
+                            An Action 4
+                        </div>
+                    </div>
+                </div>
+
+                <div className="SavedHolstersWeightAndTypeOfSet">
+                    <div className="SavedHolsterWeightOfSet">
+                        99
+                    </div>
+                    <div className="SavedHolstersTypeOfSet">
+                        Drvsh
+                    </div>
+                </div>
+            </div>
         </div>
         
     </div>
