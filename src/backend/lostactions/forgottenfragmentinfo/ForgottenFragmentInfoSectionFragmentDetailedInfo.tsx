9fcb5d5096896668427407e13e78907347a511ef
K-Kocak <kakocak@outlook.com>
2024-09-21 20:43:50 +0100

    created temporary images to replicate ingame fates and critical engagements. Clicking on a forgotten fragment will now list in a small box what actions it opens up.

--- src/backend/lostactions/forgottenfragmentinfo/ForgottenFragmentInfoSectionFragmentDetailedInfo.tsx
+++ src/backend/lostactions/forgottenfragmentinfo/ForgottenFragmentInfoSectionFragmentDetailedInfo.tsx
@@ -1,23 +1,76 @@
+import { BaseSyntheticEvent } from 'react';
+
 import { useAppSelector } from '@app/backend/hooks';
+
 import '@css/ui/components/ForgottenFragmentInfoSectionFragmentDetailedInfo.scss';
+
 import ForgottenFragmentsAsObjectArray from '@backend/lostactions/forgottenfragmentdata/ForgottenFragmentDataToObjectArray';
+
+import ForgottenFragmentLostActionList from './ForgottenFragmentInfoSectionFragmentListActions';
+
+import FateBossImage from '@ui/pictures/TemporaryFateCEIcons/FateBossIcon.png';
+import FateFightImage from '@ui/pictures/TemporaryFateCEIcons/FateFightIcon.png';
+import FateDefenseImage from '@ui/pictures/TemporaryFateCEIcons/FateDefenseIcon.png';
+import FateCollectImage from '@ui/pictures/TemporaryFateCEIcons/FateCollectionIcon.png';
+import CEBossImage from '@ui/pictures/TemporaryFateCEIcons/CEBossIcon.png';
+import CEWaveImage from '@ui/pictures/TemporaryFateCEIcons/CEWaveIcon.png';
+import CEDuelImage from '@ui/pictures/TemporaryFateCEIcons/CEDuelIcon.png';
+import DALCASImage from '@ui/pictures/TemporaryFateCEIcons/CastrumDalriadaRaidIcon.png';
+
+import SampleMapBG from '@ui/pictures/TemporaryFateCEIcons/FFXIVMapBackgroundSample.png';
+
 
 const GenerateForgottenFragmentDetailedInfoSection = () => {
     const currentSelectedForgottenFragment = useAppSelector((state) => state.ForgottenFragmentInfo.idOfFragmentDisplayed);
     console.log(currentSelectedForgottenFragment);
+
+    
+    if(currentSelectedForgottenFragment == -1) {
+        return <></>;
+    }
+
+    function HandleViewMapHover(event : BaseSyntheticEvent) {
+        console.log(event);
+        event.target.nextSibling.classList.toggle("hidden");
+    }
+
+    const ForgottenFragmentInUse = ForgottenFragmentsAsObjectArray[currentSelectedForgottenFragment];
+    const ForgottenFragmentLostActionListToDisplay : React.JSX.Element = ForgottenFragmentLostActionList(ForgottenFragmentInUse);
+
     return (
     <div className="ForgottenFragmentDetailedInfoContainer">
         <div className="ForgottenFragmentDetailedInfoNameImgMap">
             <div className="ForgottenFragmentDetailedInfoRightArrow">
-
+                <span>={'>'}</span>
             </div>
             <div className="ForgottenFragmentDetailedInfoImage">
-                {currentSelectedForgottenFragment != -1 ? <img src={ForgottenFragmentsAsObjectArray[currentSelectedForgottenFragment].img}></img> : <></>}
+                {<img src={ForgottenFragmentInUse.img}></img>}
             </div>
             <div className="ForgottenFragmentDetailedInfoName">
-
+                <span>{ForgottenFragmentInUse.name}</span>
             </div>
             <div className="ForgottenFragmentDetailedInfoMap">
+                <span onMouseEnter={HandleViewMapHover} onMouseLeave={HandleViewMapHover}>View Map</span>
+                <div className="ForgottenFragmentDetailedInfoMapImg">
+                <img className="TempBG" src={SampleMapBG}></img>
+                    <div className="Temp">
+                        
+                        <img className="Top" src={FateBossImage}></img>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div className="ForgottenFragmentDetailedInfoActionsAndLootDrops">
+            <div className="ForgottenFragmentDetailedInfoActions">
+                <div className="ForgottenFragmentDetailedInfoAppraisal">
+                    <span>Appraise at rank<span className="ForgottenFragmentDetailedInfoAppraisalRank"> {ForgottenFragmentInUse.rank}</span>  for . . .</span>
+                </div>
+                {ForgottenFragmentLostActionListToDisplay}
+            </div>
+            <div className="ForgottenFragmentDetailedInfoLootDrops">
+
             </div>
         </div>
     </div>
---
+++ src/backend/lostactions/forgottenfragmentinfo/ForgottenFragmentInfoSectionFragmentListActions.tsx
@@ -0,0 +1,34 @@
+import IForgottenFragment from "@app/backend/interfaces/IForgottenFragment";
+
+import '@css/ui/components/ForgottenFragmentInfoSectionFragmentListActions.scss'
+
+import LostActionsAsObjectArray from "@backend/lostactions/actiondata/ActionDataToObjectArray";
+import IAction from "@backend/interfaces/IAction";
+
+
+const CreateLostActionRow = (LostAction : IAction) : React.JSX.Element => {
+    return (
+        <div className="ForgottenFragmentDetailedInfoLostActionRow">
+            <div className="ForgottenFragmentDetailedInfoLostActionRowImage">
+                <img src={LostAction.img}></img>
+            </div>
+            <div className="ForgottenFragmentDetailedInfoLostActionRowName">
+                <span>{LostAction.name.EN}</span>
+            </div>
+        </div>
+    )
+}
+
+
+const ForgottenFragmentLostActionList = (ForgottenFragment : IForgottenFragment) : React.JSX.Element => {
+
+    const LostActionList : React.JSX.Element[] = [];
+    ForgottenFragment.descriptionContainsLostAction.forEach((IdOfLostAction) => {
+        const LostActionRowToPush = CreateLostActionRow(LostActionsAsObjectArray[IdOfLostAction]);
+        LostActionList.push(LostActionRowToPush);
+    })
+    return (<div className="ForgottenFragmentDetailedInfoActionList">
+        {LostActionList}
+    </div>)
+}
+export default ForgottenFragmentLostActionList
