f528cb5c5cc4bedf08150a283e04926ca2133552
K-Kocak <kakocak@outlook.com>
2024-09-25 20:09:39 +0100

    Setting up interfaces for prepop actions and boss timeline

---
+++ src/backend/interfaces/IHolsterTimeline.ts
@@ -0,0 +1,167 @@
+/*export interface ISlottedActionsAndEssence {
+    DutyActionLeft: number,
+    DutyActionRight: number,
+    DutyEssence: number
+}
+
+interface IStateOfSlottedActionsAndEssence extends ISlottedActionsAndEssence {
+    TimeOfSlottedActionState: string
+}
+
+interface IActionInteraction {
+    ActionToChange: number,
+    ActionToPlace: number,
+}
+
+interface IActionInteractionOutOfCombat extends IActionInteraction {
+    isMimic: boolean
+}
+
+interface IOutOfCombatActionsTaken {
+    StateOfActions: IStateOfSlottedActionsAndEssence[],
+    StateOfActionsModifications: IActionInteractionOutOfCombat[]
+}
+
+interface IInCombatActionsTaken {
+    StateOfActions: IStateOfSlottedActionsAndEssence[],
+    StateOfActionsModifications: IActionInteraction[]
+}
+
+interface IBossEncounter {
+    PreBoss: IOutOfCombatActionsTaken,
+    BossFight: IInCombatActionsTaken,
+    PostBoss: IOutOfCombatActionsTaken
+}
+
+export interface IHolsterTimeline {
+    Prepop : ISlottedActionsAndEssence
+    HolsterTimeline: IBossEncounter[]
+}
+
+export const SomeCrap : IHolsterTimeline = {
+    Prepop: {
+        DutyActionLeft: 103,
+        DutyActionRight: 305,
+        DutyEssence: 715
+    },
+
+    HolsterTimeline: [{
+
+        PreBoss: {
+
+            StateOfActions: [{
+                DutyActionLeft: 103,
+                DutyActionRight: 305,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: ""
+            }, {
+                DutyActionLeft: 103,
+                DutyActionRight: 306,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: "-30s"
+            }],
+            
+
+            StateOfActionsModifications: [{
+                isMimic: false,
+                ActionToChange: 305,
+                ActionToPlace: 306,
+            }]
+
+        },
+        BossFight: {
+            StateOfActions: [{
+                DutyActionLeft: 103,
+                DutyActionRight: 306,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: ""
+            }, {
+                DutyActionLeft: 103,
+                DutyActionRight: 202,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: "5s"
+            }],
+
+            StateOfActionsModifications: [{
+                ActionToChange: 306,
+                ActionToPlace: 202,
+            }]
+        },
+        PostBoss: {
+            StateOfActions: [{
+                DutyActionLeft: 103,
+                DutyActionRight: 305,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: ""
+            }, {
+                DutyActionLeft: 103,
+                DutyActionRight: 306,
+                DutyEssence: 715,
+                TimeOfSlottedActionState: "-30s"
+            }],
+            
+
+            StateOfActionsModifications: [{
+                isMimic: false,
+                ActionToChange: 305,
+                ActionToPlace: 306,
+            }]
+        }
+    }]
+};
+
+const TestCrap : IHolsterTimeline = {
+    Prepop: {
+        DutyActionLeft: 103,
+        DutyActionRight: 305,
+        DutyEssence: 715,
+    },
+    HolsterTimeline: []
+}
+
+const PreBoss : IOutOfCombatActionsTaken = {
+   StateOfActions: [{...TestCrap.Prepop, TimeOfSlottedActionState: "N/A"}],
+   StateOfActionsModifications: []
+}
+
+const BossFight : IInCombatActionsTaken = {
+    StateOfActions: [{...PreBoss.StateOfActions[PreBoss.StateOfActions.length-1]}],
+    StateOfActionsModifications: []
+}
+
+const PostBoss : IOutOfCombatActionsTaken = {
+    StateOfActions: [{...BossFight.StateOfActions[PreBoss.StateOfActions.length-1]}],
+    StateOfActionsModifications: []
+ }
+ 
+const BossToAdd : IBossEncounter = {
+    PreBoss: PreBoss,
+    BossFight: BossFight,
+    PostBoss: PostBoss
+}
+
+TestCrap.HolsterTimeline.push(BossToAdd);
+export default IHolsterTimeline;*/
+
+export interface UserSlottedActions {
+    LostActionLeft: number,
+    LostActionRight: number,
+    EssenceInUse: number
+}
+
+interface LostActionExpenditure {
+    LostActionUsed: number,
+    LostActionTimeOfUse: number
+}
+
+interface IEncounter {
+    PullBossWith: UserSlottedActions,
+    LostActionsSpentInPull: LostActionExpenditure[],
+    LostActionsSpentAfterPull: LostActionExpenditure[]
+}
+
+export interface IHolsterTimeline {
+    Encounters: IEncounter[]
+}
+
+export default IHolsterTimeline
