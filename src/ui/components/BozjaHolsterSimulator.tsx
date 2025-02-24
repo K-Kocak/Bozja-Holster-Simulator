c80ce6af1e19944be89ee537dd252b71555c59cd
K-Kocak <kakocak@outlook.com>
2025-02-24 14:01:15 +0000

    Fix unused var so netlify can build

--- src/ui/components/BozjaHolsterSimulator.tsx
+++ src/ui/components/BozjaHolsterSimulator.tsx
@@ -22,6 +22,7 @@ function BozjaHolsterSimulator() {
         const testConnection = fetch("http://localhost:5000/api")
         .then(response => response.json())
         .then(data => console.log(data));
+        console.log(testConnection);
     }, []);
     /*
     TEST CODE FOR IMPLEMENTING LINKS FOR SAVED SETS
